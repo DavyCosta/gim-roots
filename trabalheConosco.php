cad84558788dc28cbfee2155ea8ce6bbe8dc44b2
Davy <davycabral2014@gmail.com>
2022-08-02 17:44:26 -0300

    atualizando site

--- trabalheConosco.php
+++ trabalheConosco.php
@@ -25,9 +25,6 @@
     <!-- javaScript Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
     </script>
-
-    <!-- JavaScript do projeto -->
-    <script src="./js/script.js"></script>
 </head>
 
 <body>
@@ -38,7 +35,6 @@
                 <img src="./img/logoGim.png" alt="logo">
                 <span>Gim Roots </span>
             </a>
-
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-items" aria-controls="navbar-items" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="bi bi-list"></i>
@@ -63,7 +59,7 @@
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a href="#featured-container" class="nav-link primary-color">Novidades</a>
+                        <a href="novidades.php" class="nav-link primary-color">Novidades</a>
                     </li>
                     <li class="nav-item">
                         <a href="#footer-bottom" class="nav-link primary-color">Contatos</a>
@@ -71,192 +67,64 @@
 
                 </ul>
                 <div class="nav-item col-2">
-                    <a href="#" class="bi bi-heart primary-color check"></a>
-                    <a href="#" class="bi bi-cart2 primary-color check position-relative">
-                        <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
-                            <span class="visually-hidden">New alerts</span>
-                        </span>
-                    </a>
+                    <a href="favoritos.php" class="bi bi-heart primary-color check"></a>
+                    <a href="carrinho.php" class="bi bi-cart2 primary-color check"></a>
                 </div>
             </div>
         </div>
 
     </nav>
 
-    <!-- SLIDER -->
-    <header>
-        <div class="container-fluid" id="slider-container">
-            <div class="carousel slide " id="slider" data-bs-ride="carousel">
-                <div class="carousel-indicators ">
-                    <button type="button" class="active" data-bs-target="#slider" data-bs-slide-to="0" aria-current="true" aria-label="Slide 1">
-                    </button>
-                    <button type="button" data-bs-target="#slider" data-bs-slide-to="1" aria-label="Slide 2">
-                    </button>
-                    <button type="button" data-bs-target="#slider" data-bs-slide-to="2" aria-label="Slide 3">
-                    </button>
-                </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="img/fundo1.jpg" alt="Imagem 1" class="d-block w-100">
-                        <div class="carousel-caption">
-                            <a href="#" class="btn btn-dark">Ver projetos</a>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="img/fundo2.jpg" alt="Imagem 2" class="d-block w-100">
-                        <div class="carousel-caption">
-                            <a href="#" class="btn btn-dark">Ver projetos</a>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="img/fundo3.png" alt="Imagem 3" class="d-block w-100">
-                        <div class="carousel-caption">
-                            <a href="#" class="btn btn-dark">Ver projetos</a>
-                        </div>
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#slider" data-bs-slide="prev">
-                    <i class="bi bi-chevron-left"></i>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#slider" data-bs-slide="next">
-                    <i class="bi bi-chevron-right"></i>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-            <!-- MINI BANNERS -->
-            <div class="col-12 col-md-10 offset-md-1" id="mini-banners">
-                <div class="row">
-                    <div class="col-12 col-md-4">
-                        <div class="card text-center">
-                            <i class="bi bi-shop primary-color"></i>
-                            <div class="card-body">
-                                <h4 class="card-title primary-color">
-                                    Uniformes
-                                </h4>
-                                <p class="card-text secondary-color">
-                                    Faça o uniforme de sua empresa.
-                                </p>
-                                <a href="#" class="btn btn-dark btn-s">Saber Mais</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4">
-                        <div class="card text-center">
-                            <i class="bi bi-brush primary-color"></i>
-                            <div class="card-body">
-                                <h4 class="card-title primary-color">
-                                    Personalizadas
-                                </h4>
-                                <p class="card-text secondary-color">
-                                    personalize camiseta com a sua arte.
-                                </p>
-                                <a href="#" class="btn btn-dark btn-s">Saber Mais</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4">
-                        <div class="card text-center">
-                            <i class="bi bi-geo-alt primary-color "></i>
-                            <div class="card-body">
-                                <h4 class="card-title primary-color">
-                                    Entrega
-                                </h4>
-                                <p class="card-text secondary-color">
-                                    Entregamos para todas as regiões.
-                                </p>
-                                <a href="#" class="btn btn-dark btn-s">Saber Mais</a>
-                            </div>
-                        </div>
-                    </div>
 
-                </div>
-            </div>
+    <!-- POLITICA DE TROCAS -->
+    <header class="container" id="topic-container">
+        <div class="col-12 title">
+            <h1>
+                TRABALHE CONOSCO
+            </h1>
         </div>
+        <div class="topic">
+            <p>
+                Preencha as informações abaixo para concluir o cadastro:
+            <p>
+            <form class="row g-4">
+                <div class="col-md-10">
+                    <label for="validationServer01" class="form-label">Nome Completo</label>
+                    <input type="text" class="form-control" value="" required placeholder="Gabriel da Silva">
 
-        <!-- DESTAQUES -->
-        <div class="container" id="featured-container">
-            <div class="col-12 title">
-                <h1>Novidades</h1>
-                <p>
-                    Conheça nossos produtos com as melhores qualidades.
-                </p>
-            </div>
-            <div class="col-12" id="featured-images">
-                <div class="row g-4">
-                    <div class="col-12 col-md-4">
-                        <img src="img/touca.png" alt="Projeto 1" class="img-fluid">
-                        <div class="banner-content">
-                            <p class="secondary-color">Eu quero</p>
-                            <h3>Toucas</h3>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4">
-                        <img src="img/camisa.png" alt="Projeto 1" class="img-fluid">
-                        <div class="banner-content">
-                            <p class="secondary-color">Eu quero</p>
-                            <h3>Camisetas</h3>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4">
-                        <img src="img/blusa.png" alt="Projeto 1" class="img-fluid">
-                        <div class="banner-content">
-                            <p class="secondary-color">Eu quero</p>
-                            <h3>Blusas</h3>
-                        </div>
+                </div>
+                <div class="col-md-10">
+                    <label for="validationServer02" class="form-label">Telefone</label>
+                    <input type="text" class="form-control" value="" required required placeholder="11 98456-6511">
+                </div>
+                <div class="col-md-10">
+                    <label for="validationServerUsername" class="form-label">E-mail</label>
+                    <div class="input-group has-validation">
+                        <span class="input-group-text" id="inputGroupPrepend3">@</span>
+                        <input type="text" class="form-control" aria-describedby="inputGroupPrepend3 " required required placeholder="name@example.com">
+
                     </div>
                 </div>
-            </div>
-        </div>
-
-        <!-- INFO -->
-        <div class="container" id="info-container">
-            <div class="col-12 title">
-                <h1>Detalhes Importantes</h1>
-                <p>
-                    Saiba mias sobre a incrivel estamparia que inicia uma história
-                </p>
-            </div>
-            <div class="col-12">
-                <div class="row">
-                    <div class="col-12 col-md-5" id="info-banner">
-                        <img src="img/fundo4.jpg" alt="Informações da Empresa" class="img-fluid">
-                    </div>
-                    <div class="col-12 col-md-7 bg-secondary-color" id="info-content">
-                        <div class="row">
-                            <div class="col-12 title">
-                                <h1>
-                                    Gim Roots
-                                </h1>
-                                <p>
-                                    A Estamparia Gim Roots é sinônimo de qualidade desde 2021 e
-                                    atende os principais requisitos de mercado nacional e internacional.
-                                    Uma empresa pertencente ao estilo jovem, considerado o uma marca
-                                    de otima qualidade e acabamento.
-                                </p>
-                            </div>
-                            <div class="col-12" id="info-mvv">
-                                <div class="row">
-                                    <div class="col-4">
-                                        <h3 class="green">Missão</h3>
-                                        <p class="primary-color">Levar inspiração e inovação para todas as pessoas do mundo.</p>
-                                    </div>
-                                    <div class="col-4">
-                                        <h3 class="yellow">Visão</h3>
-                                        <p class="primary-color">Ser uma referência em roupas de Streetwear mantendo assim um vínculo com estilo de vida das pessoas.</p>
-                                    </div>
-                                    <div class="col-4">
-                                        <h3 class="red">Valores</h3>
-                                        <p class="primary-color">Empresa inovadora que proporciona modernidade, atendendo assim a necessidade do público.</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                <div class="col-md-10">
+                    <label for="formFile" class="form-label">Curriculo</label>
+                    <input class="form-control" type="file" id="formFile">
+                </div>
+                <div class="col-md-10">
+                    <label for="validationServerUsername" class="form-label">Mensagem</label>
+                    <textarea class="form-control" id="validationTextarea" placeholder="Required example textarea" required=""></textarea>
+                    <div class="invalid-feedback">
+                        Digite sua mensagem aqui.
                     </div>
                 </div>
-            </div>
+                <div class="d-grid  col-3">
+                    <button class="btn btn-dark btn-s" type="submit">Enviar</button>
+                </div>
+            </form>
+
         </div>
     </header>
+
+
     <!-- NEWSLETTER -->
     <div class="container-fluid container-newsletter">
         <div class="container">
